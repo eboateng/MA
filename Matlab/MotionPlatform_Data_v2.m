65a54359199ac68dc47f79ea1644f5c7cb3fcb43
eboateng <e.mensahboateng@yahoo.co.uk>
2023-11-01 23:23:07 +0100

    added new script, scapped data

--- Matlab/MotionPlatform_Data_v2.m
+++ Matlab/MotionPlatform_Data_v2.m
@@ -1,6 +1,6 @@
 % Specify the file paths for the two CSV files
 file1 = '../Data/motion_data.csv';  % File 1
-file2 = '../Data/Motion_Measurement2.csv';  % File 2
+file2 = '../Data/Motion_Measurement.csv';  % File 2
 
 % Read data from the CSV file
 data = readtable(file1);
@@ -8,7 +8,7 @@ data.Properties.VariableNames = {'Time', 'Roll (rad)', 'Pitch (rad)', 'Yaw (rad)
 
 % Read data from the second CSV file
 data2 = readtable(file2);
-data2.Properties.VariableNames = {'Timestamp','Roll(deg)','Pitch(deg)','Yaw(deg)', 'Roll (rad)', 'Pitch (rad)', 'Yaw (rad)'};
+data2.Properties.VariableNames = {'Timestamp','Roll(deg)','Pitch(deg)','Yaw(deg)'};%, 'Roll (rad)', 'Pitch (rad)', 'Yaw (rad)'};
 
 
 % Extract relevant data
@@ -24,12 +24,10 @@ time2 = datenum(data2.('Timestamp'));  % Convert the time string
 roll2 = data2.('Roll(deg)');
 pitch2 = data2.('Pitch(deg)');
 yaw2 = data2.('Yaw(deg)');
-roll3 = rad2deg(data2.('Roll (rad)'));
-pitch3 = rad2deg(data2.('Pitch (rad)'));
-yaw3 = rad2deg(data2.('Yaw (rad)'));
+% roll3 = rad2deg(data2.('Roll (rad)'));
+% pitch3 = rad2deg(data2.('Pitch (rad)'));
+% yaw3 = rad2deg(data2.('Yaw (rad)'));
 
-% try to synchromize timestamps
-newTable = synchronize(time, time2,'commonrange', 'linear')
 % Plot both files over time
 figure;
 
@@ -37,7 +35,7 @@ figure;
 plot(time, roll, 'r', 'LineWidth', 0.5);
 hold on;
 %plot(time, roll, 'b--', 'LineWidth', 0.5);
-plot(time2, pitch2, 'g', 'LineWidth', 0.5);
+plot(time2, roll2, 'g', 'LineWidth', 0.5);
 xlabel('Time');
 ylabel('Angle (radians/degrees)');
 title('fkRoll and Roll (IMU)');
